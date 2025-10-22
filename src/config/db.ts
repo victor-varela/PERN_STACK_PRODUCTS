b2339453eb0a90a2b1f9488ed8ea949ec8664e55
Victor Varela <victor398@gmail.com>
2025-10-22 12:54:09 -0300

    fix: remove sensitive database connection details from .env and update documentation

--- src/config/db.ts
+++ src/config/db.ts
@@ -17,4 +17,6 @@ db = sequelize
 - El string connection es una dato sensible debe ir en una variable de entorno. Para eso usamos la dependency dotenv--> esto facilita leer las variables que se crean en .env
 - Para crear una instancia de dotenv hay que ejecutar dotenv.config({path:'./src/.env'}) para darle la ruta del .env y poder usar la configuracion de ssl.
 - Recuerda agregar en gitignore el .env --> crea el archivo en la raiz del proyecto .gitignore
+
+- Se armo tremendo problema porque ya habia pusheado este archivo con los datos de la DB expuesto. Para arreglarlo estuve 2 dias con el chat y al final lo resolvi haciendo un git rebase y cuando salio el conflicto lei lo que proponia git e hice git add /src/config/db.ts (que es el archivo que estaba correcto) y luego git push force.. ahi se elimino el commit de github y ya no lo puede ver nadie.
 */
